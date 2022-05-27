22e9ab8c748be840923ba7289b8f0cecaa90211d
DoctorDevTool <drruummer@gmail.com>
2022-05-27 14:39:07 +0300

    added index-main-collection.js

---
+++ java-script/index-main-collection.js
@@ -0,0 +1,45 @@
+(function () {
+    const tabWomen = document.querySelector('.main-collection-main__women');
+    const tabMen = document.querySelector('.main-collection-main__men');
+    const tabShoes = document.querySelector('.main-collection-main__shoes');
+    const buttonWomen = document.querySelector('.tab-women');
+    const buttonMen = document.querySelector('.tab-men');
+    const buttonShoes = document.querySelector('.tab-shoes');
+
+    const tabs = [tabWomen, tabMen, tabShoes];
+
+    let i = 0;
+    let j = 1;
+    let k = 2;
+
+    function showTab() {
+        tabs[i].classList.add('tabs-block-activator');
+        tabs[j].classList.remove('tabs-block-activator');
+        tabs[k].classList.remove('tabs-block-activator');
+    }
+     showTab();
+    function womenShow() {
+        i = 0;
+        j = 1;
+        k = 2;
+        showTab()
+    }
+    function menShow() {
+        i = 1;
+        j = 2;
+        k = 0;
+        showTab()
+    }
+    function shoesShow() {
+        i = 2;
+        j = 0;
+        k = 1;
+        showTab()
+    }
+
+    buttonWomen.addEventListener('click', womenShow);
+    buttonMen.addEventListener('click', menShow);
+    buttonShoes.addEventListener('click', shoesShow)
+
+    console.log(menShow());
+})();
