b4e372ac97816b855727c9c9ae7198bb8f383240
hrytsenko92 <m.hrytsenko1802@gmail.com>
2022-05-30 16:04:34 +0300

    fix new-arrivals grid

--- java-script/new-arrivals__slider.js
+++ java-script/new-arrivals__slider.js
@@ -1,7 +1,8 @@
 (function () {
   const slides = [
     `<article class="new-arrivals__slider__item_1 card">
-        <img src="img/offer_new-arrivals/nike.jpeg" alt="" class="slider__item_img" />
+        <a href="nike-winter-jacket.html">
+        <img src="img/offer_new-arrivals/nike.jpeg" alt="" class="slider__item_img" /></a>
         <h4>
           <a href="nike-winter-jacket.html" class="slider__item_span-name-a"
             >Nike Winter Jacket in Blue</a
@@ -11,7 +12,8 @@
         <a class="slider__item_btn_a">Add to Cart</a>
             </article>`,
     `<article class="new-arrivals__slider__item_2 card">
-        <img src="img/offer_new-arrivals/adidas.jpeg" alt="" class="slider__item_img" />
+        <a href="adidas-black-trainers.html">
+        <img src="img/offer_new-arrivals/adidas.jpeg" alt="" class="slider__item_img" /></a>
         <h4>
           <a href="adidas-black-trainers.html" class="slider__item_span-name-a"
             >Adidas Black Trainers</a
@@ -21,7 +23,8 @@
         <a class="slider__item_btn_a">Add to Cart</a>
             </article>`,
     `<article class="new-arrivals__slider__item_3 card">
-      <img src="img/offer_new-arrivals/reebok.jpeg" alt="" class="slider__item_img" />
+      <a href="reebok-red-duster.html">
+      <img src="img/offer_new-arrivals/reebok.jpeg" alt="" class="slider__item_img" /></a>
       <h4>
         <a href="reebok-red-duster.html" class="slider__item_span-name-a"
           >Reebok Red Duster</a
@@ -31,7 +34,8 @@
       <a class="slider__item_btn_a">Add to Cart</a>
             </article>`,
     `<article class="new-arrivals__slider__item_4 card">
-        <img src="img/offer_new-arrivals/asics.jpeg" alt="" class="slider__item_img" />
+        <a href="asics-jordan-2000.html">
+        <img src="img/offer_new-arrivals/asics.jpeg" alt="" class="slider__item_img" /></a>
         <h4>
           <a href="asics-jordan-2000.html" class="slider__item_span-name-a"
             >Asics Jordan 2000</a
@@ -41,7 +45,8 @@
         <a class="slider__item_btn_a">Add to Cart</a>
             </article>`,
     `<article class="new-arrivals__slider__item_5 card">
-        <img src="img/offer_new-arrivals/puma.jpeg" alt="" class="slider__item_img" />
+      <a href="puma-textile-running-shoes.html">
+        <img src="img/offer_new-arrivals/puma.jpeg" alt="" class="slider__item_img" /></a>
         <h4>
           <a href="puma-textile-running-shoes.html" class="slider__item_span-name-a"
             >Puma Textile Running Shoes</a
@@ -66,7 +71,7 @@
         slides[currentSlide + 1] != undefined
           ? slides[currentSlide + 1]
           : slides[0];
-    } else if (window.innerWidth > 999) {
+    } else if (window.innerWidth > 945) {
       const secondSlide =
         currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
       const thirdSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1;
