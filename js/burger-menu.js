b76f603b33a71bae75324c7d3ad43e0ede2c7640
Dasha <d.denderuk@gmail.com>
2022-05-31 15:34:11 +0300

    burger menu

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -5,21 +5,27 @@
         const social = document.getElementById("social-icon")
         const link = document.querySelectorAll(".header_top-nav-menu a")
 
-        if (content.style.display = "none") {
-            buttonClick.addEventListener("click", () => {
-                content.style.display = "block"
-                document.body.style.overflow = "hidden";
-            })}
-            // buttonClick.removeEventListener("click", () => {
-            //     document.body.style.overflow = "scroll";
-            //     content.style.display = "none"
-            // });
-        for (const linkH of link){
+
+        // buttonClick.addEventListener("click", () => {
+        //     content.style.display = "block"
+        //     document.body.style.overflow = "hidden";
+        // })
+
+        // if (content.style.display = "block"){
+        //     buttonClick.addEventListener("click", () => {
+        //         document.body.style.overflow = "scroll";
+        //     });
+        // }
+
+        for (const linkH of link) {
             linkH.addEventListener("click", () => {
-            content.style.display = "none"
-            social.style.display = "none"
-            document.body.style.overflow = "scroll";
-        })};
+                content.style.display = "none"
+                social.style.display = "none"
+                document.body.style.overflow = "scroll";
+            })
+        }
+        ;
     }
+
     burgerMenu();
 })();
